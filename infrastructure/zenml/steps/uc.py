671f9d040f187d1e4a07a9c41978a11160c07787
tsunghaochen <tsunghao.chen@axa.com>
2025-04-22 21:58:02 +0200

    add poetry and pyenv dependencies

--- infrastructure/zenml/steps/uc.py
+++ infrastructure/zenml/steps/uc.py
@@ -1,7 +1,24 @@
+import urllib3
 from zenml import step
 from zenml.logger import get_logger
 from apps.uc_pipeline.uc import my_use_case
 import mlflow
+
+from urllib3.util.ssl_ import create_urllib3_context
+
+# Monkey-patch urllib3 to disable SSL verification
+def disable_ssl_verification():
+    """Disable SSL verification globally for urllib3."""
+    def patched_create_urllib3_context(*args, **kwargs):
+        context = create_urllib3_context(*args, **kwargs)
+        context.check_hostname = False
+        context.verify_mode = False
+        return context
+
+    urllib3.connection.create_urllib3_context = patched_create_urllib3_context
+
+disable_ssl_verification()
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 logger = get_logger(__name__)
 
